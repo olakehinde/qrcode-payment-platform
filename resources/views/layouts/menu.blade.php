f36e3ca667d8d98c50608cc129f66efac13091db
Olamide <olakehinde59@gmail.com>
2019-05-28 12:38:52 +0100

    added icons to the side bar menu items

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,16 +1,16 @@
 <li class="{{ Request::is('qrcodes*') ? 'active' : '' }}">
-    <a href="{!! route('qrcodes.index') !!}"><i class="fa fa-edit"></i><span>Qrcodes</span></a>
+    <a href="{!! route('qrcodes.index') !!}"><i class="fa fa-qrcode"></i><span>Qrcodes</span></a>
 </li>
 
 <li class="{{ Request::is('roles*') ? 'active' : '' }}">
-    <a href="{!! route('roles.index') !!}"><i class="fa fa-edit"></i><span>Roles</span></a>
+    <a href="{!! route('roles.index') !!}"><i class="fa fa-user"></i><span>Roles</span></a>
 </li>
 
 <li class="{{ Request::is('transactions*') ? 'active' : '' }}">
-    <a href="{!! route('transactions.index') !!}"><i class="fa fa-edit"></i><span>Transactions</span></a>
+    <a href="{!! route('transactions.index') !!}"><i class="fa fa-arrow-up"></i><span>Transactions</span></a>
 </li>
 
 <li class="{{ Request::is('users*') ? 'active' : '' }}">
-    <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>Users</span></a>
+    <a href="{!! route('users.index') !!}"><i class="fa fa-users"></i><span>Users</span></a>
 </li>
 
