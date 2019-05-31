56396ffd82222920bc1c95898adbb1b668e7085e
Olamide <olakehinde59@gmail.com>
2019-05-31 10:51:16 +0100

    did minor adjustments on the create qrcode page. put placeholder, set user_id as hidden

--- resources/views/qrcodes/fields.blade.php
+++ resources/views/qrcodes/fields.blade.php
@@ -1,13 +1,10 @@
 <!-- User Id Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('user_id', 'User Id:') !!}
-    {!! Form::number('user_id', null, ['class' => 'form-control']) !!}
-</div>
+{!! Form::hidden('user_id', Auth::user()->id, ['class' => 'form-control']) !!}
 
 <!-- Website Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('website', 'Website:') !!}
-    {!! Form::text('website', null, ['class' => 'form-control']) !!}
+    {!! Form::text('website', null, ['class' => 'form-control', 'placeholder' => 'https://']) !!}
 </div>
 
 <!-- Company Name Field -->
@@ -35,14 +32,14 @@
 </div>
 
 <!-- Qrcode Path Field -->
-<div class="form-group col-sm-6">
+<!-- <div class="form-group col-sm-6">
     {!! Form::label('qrcode_path', 'Qrcode Path:') !!}
     {!! Form::text('qrcode_path', null, ['class' => 'form-control']) !!}
-</div>
+</div> -->
 
 <!-- Amount Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('amount', 'Amount:') !!}
+    {!! Form::label('amount', 'Amount (#):') !!}
     {!! Form::number('amount', null, ['class' => 'form-control']) !!}
 </div>
 
@@ -51,12 +48,11 @@
     {!! Form::label('status', 'Status:') !!}
     <label class="checkbox-inline">
         {!! Form::hidden('status', 0) !!}
-        {!! Form::checkbox('status', '1', null) !!} 1
+        {!! Form::checkbox('status', '1', null) !!} Active
     </label>
 </div>
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
     {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('qrcodes.index') !!}" class="btn btn-default">Cancel</a>
 </div>
