68bd178c0d04c57f8e6a2105d60b7d82c9838b6d
Olamide <olakehinde59@gmail.com>
2019-06-10 12:16:45 +0100

    changed the label name for role

--- resources/views/roles/show_fields.blade.php
+++ resources/views/roles/show_fields.blade.php
@@ -1,12 +1,12 @@
 <!-- Name Field -->
 <div class="form-group">
-    {!! Form::label('name', 'Name:') !!}
+    {!! Form::label('name', 'Role:') !!}
     <p>{!! $role->name !!}</p>
 </div>
 
 <!-- Created At Field -->
 <div class="form-group">
-    {!! Form::label('created_at', 'Created At:') !!}
-    <p>{!! $role->created_at->format('D d, M, Y') !!}</p>
+    {!! Form::label('created_at', 'Created:') !!}
+    <p>{!! $role->created_at->format('D d M, Y') !!}</p>
 </div>
 
