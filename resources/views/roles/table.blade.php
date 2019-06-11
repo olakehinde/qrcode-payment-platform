5715fa0ced9fdf58b8dd46ad7792617c788fd230
Olamide <olakehinde59@gmail.com>
2019-06-11 15:13:45 +0100

    removed the view and delete button so roles cannot be deleted

--- resources/views/roles/table.blade.php
+++ resources/views/roles/table.blade.php
@@ -15,9 +15,9 @@
                 <td>
                     {!! Form::open(['route' => ['roles.destroy', $role->id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
-                        <a href="{!! route('roles.show', [$role->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                        <a href="{!! route('roles.edit', [$role->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                        {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                        <!-- <a href="{!! route('roles.show', [$role->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a> -->
+                        <a href="{!! route('roles.edit', [$role->id]) !!}" class='btn btn-info'><i class="glyphicon glyphicon-edit"></i> Edit</a>
+                        <!-- {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!} -->
                     </div>
                     {!! Form::close() !!}
                 </td>
