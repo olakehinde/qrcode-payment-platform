918714ce985015f2fdd9b591ccb593d14da7fc0e
Olamide <olakehinde59@gmail.com>
2019-06-11 09:28:19 +0100

    cleaned up the user index page

--- resources/views/users/table.blade.php
+++ resources/views/users/table.blade.php
@@ -3,23 +3,21 @@
         <thead>
             <tr>
                 <th>Name</th>
-        <th>Role Id</th>
-        <th>Email</th>
-        <th>Email Verified At</th>
-        <th>Password</th>
-        <th>Remember Token</th>
+                <th>Role</th>
+                <th>Email</th>
                 <th colspan="3">Action</th>
             </tr>
         </thead>
         <tbody>
         @foreach($users as $user)
             <tr>
-                <td>{!! $user->name !!}</td>
-            <td>{!! $user->role_id !!}</td>
-            <td>{!! $user->email !!}</td>
-            <td>{!! $user->email_verified_at !!}</td>
-            <td>{!! $user->password !!}</td>
-            <td>{!! $user->remember_token !!}</td>
+                <td>
+                    <a href="{!! route('users.show', [$user->id]) !!}" class="text-info">
+                        <b>{!! $user->name !!}</b>
+                    </a>
+                </td>
+                <td>{!! $user->role['name'] !!}</td>
+                <td>{!! $user->email !!}</td>
                 <td>
                     {!! Form::open(['route' => ['users.destroy', $user->id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
