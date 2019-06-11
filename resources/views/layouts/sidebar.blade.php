ca3114b682e64e36e5da8e6abb62d98065fd851a
Olamide <olakehinde59@gmail.com>
2019-06-11 11:24:31 +0100

    removed the search button and profile image from side bar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,13 +5,10 @@
 
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel">
-            <div class="pull-left image">
-                <img src="http://infyom.com/images/logo/blue_logo_150x150.jpg" class="img-circle"
-                     alt="User Image"/>
-            </div>
+            <div class="pull-left"><br><br></div>
             <div class="pull-left info">
                 @if (Auth::guest())
-                <p>InfyOm</p>
+                <p>QRCode Payment Platform</p>
                 @else
                     <p>{{ Auth::user()->name}}</p>
                 @endif
@@ -21,7 +18,7 @@
         </div>
 
         <!-- search form (Optional) -->
-        <form action="#" method="get" class="sidebar-form">
+        <!-- <form action="#" method="get" class="sidebar-form">
             <div class="input-group">
                 <input type="text" name="q" class="form-control" placeholder="Search..."/>
           <span class="input-group-btn">
@@ -29,7 +26,7 @@
             </button>
           </span>
             </div>
-        </form>
+        </form> -->
         <!-- Sidebar Menu -->
 
         <ul class="sidebar-menu" data-widget="tree">
