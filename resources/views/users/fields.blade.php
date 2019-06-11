16b4934d1b68b34ec247d0a2f52aeb3fdb1cd34e
Olamide <olakehinde59@gmail.com>
2019-06-11 10:56:07 +0100

    cleaned up the user edit page. hid some fields and put access permission on the roled field for only admins and moderators only to be able to change user role

--- resources/views/users/fields.blade.php
+++ resources/views/users/fields.blade.php
@@ -4,12 +4,18 @@
     {!! Form::text('name', null, ['class' => 'form-control']) !!}
 </div>
 
-<!-- Role Id Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('role_id', 'Role Id:') !!}
-    {!! Form::number('role_id', null, ['class' => 'form-control']) !!}
-</div>
-
+@if(Auth::user()->role_id < 3)
+    <!-- Role Field -->
+    <div class="form-group col-sm-6">
+        <label for="role_id">Role:</label>
+        <select class="form-control" id="role_id">
+            <option value="{{ $user->role['id'] }}">{{ $user->role['name'] }}</option>
+            @foreach($roles as $role)
+                <option value="{{ $role->id }}">{{ $role->name }}</option>
+            @endforeach
+        </select>
+    </div>
+@endif
 <!-- Email Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('email', 'Email:') !!}
@@ -17,10 +23,7 @@
 </div>
 
 <!-- Email Verified At Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('email_verified_at', 'Email Verified At:') !!}
-    {!! Form::date('email_verified_at', null, ['class' => 'form-control','id'=>'email_verified_at']) !!}
-</div>
+{!! Form::hidden('email_verified_at', null, ['class' => 'form-control','id'=>'email_verified_at']) !!}
 
 @section('scripts')
     <script type="text/javascript">
@@ -38,10 +41,7 @@
 </div>
 
 <!-- Remember Token Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('remember_token', 'Remember Token:') !!}
-    {!! Form::text('remember_token', null, ['class' => 'form-control']) !!}
-</div>
+{!! Form::hidden('remember_token', null, ['class' => 'form-control']) !!}
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
