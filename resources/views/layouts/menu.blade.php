f119931212723a1fc452dff244b6c2d22ccad162
Olamide <olakehinde59@gmail.com>
2019-06-11 15:15:01 +0100

    rearranged the menu items and added access control permissions on who can see the menu items

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,16 +1,25 @@
-<li class="{{ Request::is('qrcodes*') ? 'active' : '' }}">
-    <a href="{!! route('qrcodes.index') !!}"><i class="fa fa-qrcode"></i><span>Qrcodes</span></a>
-</li>
-
-<li class="{{ Request::is('roles*') ? 'active' : '' }}">
-    <a href="{!! route('roles.index') !!}"><i class="fa fa-user"></i><span>Roles</span></a>
-</li>
-
+<!-- All users can access -->
 <li class="{{ Request::is('transactions*') ? 'active' : '' }}">
     <a href="{!! route('transactions.index') !!}"><i class="fa fa-line-chart"></i><span>Transactions</span></a>
 </li>
 
-<li class="{{ Request::is('users*') ? 'active' : '' }}">
-    <a href="{!! route('users.index') !!}"><i class="fa fa-users"></i><span>Users</span></a>
-</li>
+<!-- Webmaster -->
+@if(Auth::user()->role_id < 3)
+	<li class="{{ Request::is('qrcodes*') ? 'active' : '' }}">
+	    <a href="{!! route('qrcodes.index') !!}"><i class="fa fa-qrcode"></i><span>Qrcodes</span></a>
+	</li>
+@endif
 
+<!-- Moderator -->
+@if(Auth::user()->role_id < 3)
+	<li class="{{ Request::is('users*') ? 'active' : '' }}">
+	    <a href="{!! route('users.index') !!}"><i class="fa fa-users"></i><span>Users</span></a>
+	</li>
+@endif
+
+<!-- Only Admin can access -->
+@if(Auth::user()->role_id == 1)
+	<li class="{{ Request::is('roles*') ? 'active' : '' }}">
+	    <a href="{!! route('roles.index') !!}"><i class="fa fa-user"></i><span>Roles</span></a>
+	</li>
+@endif
