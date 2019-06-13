e914fe314c3236bdd0a38ae580e8da59fbe57b8a
Olamide <olakehinde59@gmail.com>
2019-06-13 12:47:30 +0100

    moved the back button out of the row div, so it can be displayed at the bottom of the page

--- resources/views/qrcodes/show.blade.php
+++ resources/views/qrcodes/show.blade.php
@@ -18,8 +18,10 @@
             <div class="box-body">
                 <div class="row" style="padding-left: 20px">
                     @include('qrcodes.show_fields')
-                    <a href="{!! route('qrcodes.index') !!}" class="btn btn-default"><i class="fa fa-arrow-left"></i> Back</a>
                 </div>
+                <a href="{!! route('qrcodes.index') !!}" class="btn btn-default">
+                    <i class="fa fa-arrow-left"></i> Back
+                </a>
             </div>
         </div>
     </div>
