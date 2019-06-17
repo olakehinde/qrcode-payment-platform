6c34314177e4c7a1e59458c3d82b50d02b29da5b
Olamide <olakehinde59@gmail.com>
2019-06-17 15:11:14 +0100

    registered the check moderator middleware in the routeMiddleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkadmin' => \App\Http\Middleware\checkAdmin::class,
+        'checkmoderator' => \App\Http\Middleware\checkModerator::class,
     ];
 
     /**
