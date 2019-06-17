2e6e923a6a8ac9fdae7dbe322bdb77a16abd4d89
Olamide <olakehinde59@gmail.com>
2019-06-17 14:18:09 +0100

    created the middleware to check if a  user is an admin

---
+++ app/Http/Middleware/checkAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+use Flash;
+
+class checkAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user()->role_id != 1) {
+            Flash::error("You do not have Admin permission");
+            return redirect('home');
+        }
+        return $next($request);
+    }
+}
