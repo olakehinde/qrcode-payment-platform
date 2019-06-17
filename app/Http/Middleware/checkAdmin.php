0e7ad0e127bdab3bde7179aafe24148bae465e34
Olamide <olakehinde59@gmail.com>
2019-06-17 15:06:52 +0100

    fixed the redirect url in the checkadmin middleware

--- app/Http/Middleware/checkAdmin.php
+++ app/Http/Middleware/checkAdmin.php
@@ -18,7 +18,7 @@ class checkAdmin
     public function handle($request, Closure $next)
     {
         if (Auth::user()->role_id != 1) {
-            Flash::error("You do not have Admin permission");
+            Flash::error("You do not have permission");
             return redirect('home');
         }
         return $next($request);
