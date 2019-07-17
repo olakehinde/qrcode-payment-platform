cbfd87d41ef1abd62a026972e91bc71de8fb2636
Olamide <olakehinde59@gmail.com>
2019-07-17 10:39:31 +0100

    added 'apply for payout' and 'pay' button

--- resources/views/accounts/show.blade.php
+++ resources/views/accounts/show.blade.php
@@ -2,10 +2,27 @@
 
 @section('content')
     <section class="content-header">
-        <h1>
+        <h1 class="pull-left">
             Account: {{ $account->id }} <small>{!! $account->applied_for_payout == 1 ? 'Payout request processing...' : '' !!}</small>
         </h1>
+
+        <h1 class="pull-right">
+            @if(Auth::user()->id == $account->user_id)
+                {!! Form::open(['route' => ['accounts.apply_for_payout', $account->id], 'method' => 'post', 'class' => 'pull-left', 'style' => 'padding-right:5px']) !!}
+                    {!! Form::button('Apply for Payout', ['type' => 'submit', 'class' => 'btn btn-info', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                {!! Form::close() !!}
+            @endif
+
+            @if(Auth::user()->role_id < 3)
+                {!! Form::open(['route' => ['accounts.confirm_pay', $account->id], 'method' => 'post', 'class' => 'pull-right']) !!}
+                    {!! Form::button('Pay', ['type' => 'submit', 'class' => 'btn btn-danger', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                {!! Form::close() !!}
+            @endif
+        </h1>
     </section>
+    
+    <div class="clearfix"></div>
+    
     <div class="content">
         <div class="box box-primary">
             <div class="box-body">
