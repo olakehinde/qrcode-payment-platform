1a0965dbe71b970c77aa120a569a8431726995dd
Olamide <olakehinde59@gmail.com>
2019-07-17 10:40:49 +0100

    minor refactoring. removed the 'edit' and 'delete' buttons on the account histories show view page

--- resources/views/account_histories/table.blade.php
+++ resources/views/account_histories/table.blade.php
@@ -2,27 +2,17 @@
     <table class="table" id="accountHistories-table">
         <thead>
             <tr>
-                <th>Account Id</th>
-        <th>User Id</th>
-        <th>Status Message</th>
-                <th colspan="3">Action</th>
+                <th>User</th>
+                <th>Account ID</th>
+                <th>Status Message</th>
             </tr>
         </thead>
         <tbody>
         @foreach($accountHistories as $accountHistory)
             <tr>
+                <td>{!! $accountHistory->user['name'] !!}</td>
                 <td>{!! $accountHistory->account_id !!}</td>
-            <td>{!! $accountHistory->user_id !!}</td>
-            <td>{!! $accountHistory->status_message !!}</td>
-                <td>
-                    {!! Form::open(['route' => ['accountHistories.destroy', $accountHistory->id], 'method' => 'delete']) !!}
-                    <div class='btn-group'>
-                        <a href="{!! route('accountHistories.show', [$accountHistory->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                        <a href="{!! route('accountHistories.edit', [$accountHistory->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                        {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
-                    </div>
-                    {!! Form::close() !!}
-                </td>
+                <td>{!! $accountHistory->status_message !!}</td>
             </tr>
         @endforeach
         </tbody>
