be22b086c6c5ecf42608b075e8097e71704451df
Olamide <olakehinde59@gmail.com>
2019-07-18 16:25:02 +0100

    added a form to process payment on paystack

--- resources/views/qrcodes/show_fields.blade.php
+++ resources/views/qrcodes/show_fields.blade.php
@@ -65,6 +65,36 @@
         <h3>Scan QR code to Pay</h3>
         <img src="{!! asset($qrcode->qrcode_path) !!}">
     </div>
+
+    <form method="POST" action="{{ route('pay') }}" accept-charset="UTF-8" class="form-horizontal" role="form">
+        <div class="row" style="margin-bottom:40px;">
+          <div class="col-md-8 col-md-offset-2">
+            <p>
+                <div>
+                    Lagos Eyo Print Tee Shirt
+                    ₦ 2,950
+                </div>
+            </p>
+            <input type="hidden" name="email" value="olakehinde59@gmail.com"> {{-- required --}}
+            <input type="hidden" name="orderID" value="{{$qrcode->id}}">
+            <input type="hidden" name="amount" value="{{$qrcode->amount}}"> {{-- required in kobo --}}
+            <input type="hidden" name="quantity" value="3">
+            <input type="hidden" name="metadata" value="{{ json_encode($array = ['key_name' => 'value',]) }}" > {{-- For other necessary things you want to add to your payload. it is optional though --}}
+            <input type="hidden" name="reference" value="{{ Paystack::genTranxRef() }}"> {{-- required --}}
+            <input type="hidden" name="key" value="{{ config('paystack.secretKey') }}"> {{-- required --}}
+            {{ csrf_field() }} {{-- works only when using laravel 5.1, 5.2 --}}
+
+             <input type="hidden" name="_token" value="{{ csrf_token() }}"> {{-- employ this in place of csrf_field only in laravel 5.0 --}}
+
+
+            <p>
+              <button class="btn btn-success btn-lg btn-block" type="submit" value="Pay Now!">
+              <i class="fa fa-plus-circle fa-lg"></i> Pay Now!
+              </button>
+            </p>
+          </div>
+        </div>
+</form>
 </div>
 
 <!-- import all transactions for this qrcode -->
